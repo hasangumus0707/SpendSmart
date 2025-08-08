870590fc8292b4d32576a16f0ed6a14db9bb4c1d
Hasan Gümüş <gumushasan764@gmail.com>
2025-08-08 16:30:49 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SpendSmart.Models;
+
+namespace SpendSmart.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult CreateEditExpense()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Expenses()
+    {
+        return View();
+    }
+
+    public IActionResult CreateEditExpenseForm(Expense model)
+    {
+        return  RedirectToAction("Index");
+    }
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
